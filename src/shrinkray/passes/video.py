92421513eb98a17e9b9fc6a8c4b4f92a5a4d0eff
Frank Plowman <post@frankplowman.com>
2025-02-08 20:35:30 +0000

    Include leading whitespace in deleted NALUs

--- src/shrinkray/passes/video.py
+++ src/shrinkray/passes/video.py
@@ -2,14 +2,28 @@ from shrinkray.passes.definitions import ReductionProblem
 from shrinkray.passes.patching import Cuts, apply_patches
 
 def find_start_codes(data: bytes, start_code: bytes) -> list[int]:
+    """Return a list of indexes into data at which the given start_code occurs."""
     start_codes = [0]
     while (pos := data.find(start_code, start_codes[-1] + 1)) != -1:
         start_codes.append(pos)
     return start_codes
 
-async def nalu_deletion(problem: ReductionProblem[bytes]) -> None:
+def find_nalus(data: bytes) -> list[int]:
+    """Return a list of indexes into data which identify the start of NAL units.
+       The start of a NALU is indicated by a start code.  The NAL unit may also
+       have some leading whitespace."""
     start_code_prefix = b"\x00\00\01"
-    nalu_starts = find_start_codes(problem.current_test_case, start_code_prefix)
+    nalu_starts = []
+    for pos in find_start_codes(data, start_code_prefix)[1:]:
+        while pos >= 0 and data[pos] == 0x00:
+            pos -= 1
+        pos += 1
+        nalu_starts.append(pos)
+    return nalu_starts
+
+async def nalu_deletion(problem: ReductionProblem[bytes]) -> None:
+    nalu_starts = find_nalus(problem.current_test_case)
     nalu_ends = nalu_starts[1:] + [len(problem.current_test_case)]
-    spans = zip(nalu_starts, nalu_ends)
-    await apply_patches(problem, Cuts(), [[s] for s in spans])
+    nalus = zip(nalu_starts, nalu_ends)
+    spans = [[nalu] for nalu in nalus]
+    await apply_patches(problem, Cuts(), spans)
